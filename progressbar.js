019730725016136bfcdcce0ca8fc3f061d4101ef
kartikeya72001 <kartikeya72001@gmail.com>
2020-09-28 21:36:43 +0530

    Updates

--- progressbar.js
+++ progressbar.js
@@ -1,7 +1,7 @@
 function move() {
     var elem = document.getElementById("myBar"); 
     var elem2 = document.getElementById("percentage");  
-    var width = 20;
+    var width = 10;
     var id = setInterval(frame, 10);
     function frame() {
       if (width >= 100) {
@@ -11,5 +11,54 @@ function move() {
         elem.style.width = width + '%'; 
         elem2.innerHTML = width * 1  + '%';
       }
+      if (width == 13){
+        var ele = document.getElementById(13);
+        ele.style.backgroundColor = "greenyellow";
+        console.log(ele)
+      }
+      else if(width == 32){
+        var ele = document.getElementById(32);
+        ele.style.backgroundColor = "greenyellow";
+        console.log(ele)
+      }
+      else if(width == 52){
+        var ele = document.getElementById(52);
+        ele.style.backgroundColor = "greenyellow";
+        console.log(ele)
+      }
+      else if(width == 71){
+        var ele = document.getElementById(71);
+        ele.style.backgroundColor = "greenyellow";
+        console.log(ele)
+      }
     }
+  }
+  function reset(){
+      document.getElementById("myBar").style.width = "10%";
+      document.getElementById(13).style.backgroundColor = "#bbb";
+      document.getElementById(32).style.backgroundColor = "#bbb";
+      document.getElementById(52).style.backgroundColor = "#bbb";
+      document.getElementById(71).style.backgroundColor = "#bbb";
+  }
+  function first(){
+    document.getElementById("myBar").style.width = "14%";
+    document.getElementById(13).style.backgroundColor = "greenyellow";
+  }
+  function second(){
+    document.getElementById("myBar").style.width = "32%";
+    document.getElementById(13).style.backgroundColor = "greenyellow";
+    document.getElementById(32).style.backgroundColor = "greenyellow";
+  }
+  function third(){
+    document.getElementById("myBar").style.width = "52%";
+    document.getElementById(13).style.backgroundColor = "greenyellow";
+    document.getElementById(32).style.backgroundColor = "greenyellow";
+    document.getElementById(52).style.backgroundColor = "greenyellow";
+  }
+  function fourth(){
+    document.getElementById("myBar").style.width = "71%";
+    document.getElementById(13).style.backgroundColor = "greenyellow";
+    document.getElementById(32).style.backgroundColor = "greenyellow";
+    document.getElementById(52).style.backgroundColor = "greenyellow";
+    document.getElementById(71).style.backgroundColor = "greenyellow";
   }
